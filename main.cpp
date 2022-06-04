0baa434a13f91594062a6005953ca5064d9e65ce
AlexHoring <alexhoring@qq.com>
2022-06-04 21:32:16 +0800

    写了一个CSV读取器类，并且添加了一些规范说明，and fix a bug

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     //main函数暂时用于测试，日后我会补充完整功能。
     std::cout << "Hello, World!" << std::endl;
-    Algos::sample_function();   //函数调用
+    Algos::sample_function("sample");   //函数调用
     CSVreader reader("news.csv");
     ofstream of("out.txt",ios::out);
     of<<reader.get_sentense()<<endl;
