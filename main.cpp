15c07a9a6ea78d22263ae1bc00a8651e2774858b
AlexHoring <alexhoring@qq.com>
2022-06-09 03:58:11 +0800

    用自己写的平衡二叉树类实现了Set，并且经过测试成功运行。

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "BinaryHeap.h"
 #include "WORK.h"
 #include "WordFilter.h"
+#include "AVLtree.h"
 #include <vector>
 using namespace std;
 
@@ -41,8 +42,27 @@ int main() {
 //    aa.heap_sort(v1.begin(),v1.end(),compare);
 //    //Algos::heap_sort(v1.begin(),v1.end());
 //    display(v1.begin(),v1.end());
-//    //调试模块的时候将下面的语句注释掉，将最后一句注释取消。调试功能逻辑的时候反之。
 
-   // return WORK::SEARCH();
+//    AVLtree<int> av;
+//    for(int i=1;i<16;++i){
+//        av.insert(i);
+//        if(i==3||i==7||i==15){
+//            av.print();
+//        }
+//    }
+//    av.erase(av.find(1));
+//    av.erase(av.find(3));
+//    av.erase(av.find(2));
+//    av.print();
+//    av.insert(3);
+//    av.insert(1);
+//    av.insert(2);
+//    av.print();
+//    av.erase(av.find(8));
+//    av.print();
+
+
+    //调试模块的时候将下面的语句注释掉，将最后一句注释取消。调试功能逻辑的时候反之。
+    //return WORK::SEARCH();
     return 0;
 }
